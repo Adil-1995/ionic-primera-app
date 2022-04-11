bd885a6142071983ab759029b99b2c992ad1c094
Adil Chair <achairadil@gmail.com>
2022-04-11 10:53:54 +0200

    fin seccion 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+/* eslint-disable no-debugger */
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page  implements OnInit{
 
   constructor() {}
+  ngOnInit(): void {
+    let numero = 20 ;
+    numero = 10;
+    numero = 1 + 20;
+   console.log(numero);
+
+  }
 
 }
